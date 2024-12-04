a48cc483a27eb8c2a558c035cc2f6b0844b6f5a8
Ameer Ishak <ameerdev25@gmail.com>
2024-12-04 19:26:47 +0800

    Delete dist directory

--- dist/config/database.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sequelize_1 = require("sequelize");
-const sequelize = new sequelize_1.Sequelize('postgres://postgres:postgres@localhost:5433/rbac');
-exports.default = sequelize;
